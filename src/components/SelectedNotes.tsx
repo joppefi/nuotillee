81b25963bbc1da9c289ae581ef318f440c0ded79
Joel Tolvanen <joel@shareway.fi>
2023-03-04 12:57:29 +0200

    fix: improved parsing on note input

--- src/components/SelectedNotes.tsx
+++ src/components/SelectedNotes.tsx
@@ -11,13 +11,56 @@ const SelectedNotes = () => {
     // @ts-ignore
     const lastChar = event.nativeEvent?.data?.toUpperCase();
 
+    const validInputChar = /([A-G]|\d|#)/g;
+    if (lastChar && !lastChar.match(validInputChar)) {
+      return;
+    }
+
     let value = event.target.value.toUpperCase();
 
-    // TODO: Add ability to add notes with octaves
-    if (value.length > 1 && lastChar && lastChar !== "#") {
+    const notesWithoutModifiers = /([A-G])/g;
+
+    // Force first char to be note
+    if (value.length === 1 && !value.match(notesWithoutModifiers)) {
+      return;
+    }
+
+    // Add space before new note
+    if (value.length > 1 && lastChar && lastChar.match(notesWithoutModifiers)) {
       const arr = Array.from(value);
-      arr.splice(-1, 0, " ");
+      if (arr.at(-2) !== " ") {
+        arr.splice(-1, 0, " ");
+      }
       value = arr.join("");
+    }
+
+    // Prevent modifier without a note
+    if (
+      lastChar &&
+      lastChar === "#" &&
+      !value.at(-2)?.match(notesWithoutModifiers)
+    ) {
+      return;
+    }
+
+    // Prevent consecutive sharp modifiers for one note
+    if (
+      lastChar &&
+      lastChar === "#" &&
+      value.length > 1 &&
+      value.at(-2) === "#"
+    ) {
+      return;
+    }
+
+    // Prevent consecutive octave modifiers for one note
+    if (lastChar && lastChar.match(/\d/g) && value.at(-2)?.match(/\d/g)) {
+      return;
+    }
+
+    // Prevent consecutive spaces caused by
+    if (!lastChar && value.length > 1 && value.at(-1) === " ") {
+      value = value.slice(0, -1);
     }
 
     setInputNotes(value);
