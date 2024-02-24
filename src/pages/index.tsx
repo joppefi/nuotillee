2dbc9fccd65b07a01e7ed8cf59c52e8b88f7d9e8
Joel Tolvanen <joel@shareway.fi>
2024-02-24 15:21:03 +0200

    style: use container for wrapping

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,19 @@ import Guitar from "@/components/Guitar";
 import Piano from "@/components/Piano";
 import ScaleSelector from "@/components/ScaleSelector";
 import SelectedNotes from "@/components/SelectedNotes";
-import { HStack, VStack } from "@chakra-ui/react";
+import { Container, HStack, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <VStack mx="20" alignItems="flex-start">
-      <Piano />
-      <Guitar />
-      <HStack alignItems="flex-start">
-        <SelectedNotes />
-        <ScaleSelector />
-      </HStack>
-    </VStack>
+    <Container maxW="8xl">
+      <VStack alignItems="flex-start">
+        <Piano />
+        <Guitar />
+        <HStack alignItems="flex-start">
+          <SelectedNotes />
+          <ScaleSelector />
+        </HStack>
+      </VStack>
+    </Container>
   );
 }
