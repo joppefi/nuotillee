7f50fc3dcd9fdf7adfa48a600790fc95cccd28bb
Joel Tolvanen <joel@shareway.fi>
2024-02-27 22:52:22 +0200

    feat: render selected notes on top bar

--- src/components/ui/TopBar.tsx
+++ src/components/ui/TopBar.tsx
@@ -1,12 +1,23 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, HStack } from "@chakra-ui/react";
 import React from "react";
-import SelectedNotes from "../SelectedNotes";
+import { useSelections } from "../SelectedContext";
+import SelectedNotes, { NoteBadge } from "../SelectedNotes";
 
 const TopBar = () => {
+  const { selectedNote, setSelectedNote } = useSelections();
+
   return (
     <Box w="full" backgroundColor="blackAlpha.500">
-      <Container maxW="4xl" justifyContent="flex-start">
-        <SelectedNotes />
+      <Container maxW="4xl" justifyContent="flex-start" py="4">
+        {selectedNote && (
+          <HStack>
+            {Array.isArray(selectedNote) ? (
+              selectedNote.map((note) => <NoteBadge key={note} note={note} />)
+            ) : (
+              <NoteBadge note={selectedNote} />
+            )}
+          </HStack>
+        )}
       </Container>
     </Box>
   );
