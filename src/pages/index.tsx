b51c97f82779e86e4d7e495a5829c203addebd0f
Joel Tolvanen <joel@shareway.fi>
2024-03-14 20:58:56 +0200

    refactor: move windowed ui to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,86 @@
-import Guitar from "@/components/LegacyGuitar";
-import Piano from "@/components/Piano";
-import ScaleSelector from "@/components/ScaleSelector";
-import SelectedNotes from "@/components/SelectedNotes";
-import { Container, HStack, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, VStack } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
-export default function Home() {
+import Workspace, { Config } from "@/components/ui/Workspace";
+import WorkspaceSelector from "@/components/ui/WorkspaceSelector";
+
+import { useWorkspaces } from "@/lib/swr";
+
+const WindowedUi = () => {
+  const [initialConfig, setInitialConfig] = useState<Config | undefined>();
+
+  const { isLoading, data, mutate } = useWorkspaces();
+
+  const newWorkspace = () => {
+    setInitialConfig({
+      id: uuidv4(),
+      title: "Untitled",
+      width: 8000,
+      height: 6000,
+      view: {
+        scale: 1,
+        position: {
+          x: -2000,
+          y: -800,
+        },
+      },
+      windows: [
+        {
+          id: uuidv4(),
+          position: {
+            x: 2300,
+            y: 800,
+          },
+          w: "8xl",
+          type: "guitar",
+          state: {},
+        },
+        {
+          id: uuidv4(),
+          position: {
+            x: 2300,
+            y: 800,
+          },
+          type: "youtube",
+          state: {
+            url: "https://www.youtube.com/embed/zrZjVWbtUt4",
+          },
+        },
+      ],
+    });
+  };
+
+  const handleExit = () => {
+    mutate();
+    setInitialConfig(undefined);
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
-    <Container maxW="8xl">
-      <VStack alignItems="flex-start">
-        <Piano />
-        <Guitar />
-        <HStack alignItems="flex-start">
-          <SelectedNotes />
-          <ScaleSelector />
-        </HStack>
+    <>
+      <VStack h="100dvh" spacing={0}>
+        {initialConfig && (
+          <Box
+            key={initialConfig.id}
+            flex="1"
+            backgroundColor="gray.100"
+            w="100%"
+            overflow="hidden"
+          >
+            <Workspace initialConfig={initialConfig} onExit={handleExit} />
+          </Box>
+        )}
       </VStack>
-    </Container>
+      <WorkspaceSelector
+        isOpen={!initialConfig}
+        workspaces={data}
+        onSelect={(config) => setInitialConfig(config)}
+        onCreate={newWorkspace}
+      />
+    </>
   );
-}
+};
+
+export default WindowedUi;
